1b6a44c4708b66bfc9499ee227625b85a18f5a59
unknown <fengchuyan@6C8F7A85617211B.internal.baidu.com>
2015-08-24 15:51:22 +0800

    list

--- src/list/listView.js
+++ src/list/listView.js
@@ -14,20 +14,29 @@ define(function (require) {
     config.domEvents = {
         'click: h4': function (ele, e) {
             dom.toggleClass(ele.parentNode, 'toggle');
+        },
+        'click: .delete-icon': function (ele, e) {
+            var itemDom = dom.closest(ele, 'dd', dom.g('main'));
+            this.emit('delete', dom.getData(itemDom, 'id'));
+            itemDom.parentNode.removeChild(itemDom);
+        },
+        'click: .edit-icon': function (ele, e) {
+            var itemDom = dom.closest(ele, 'dd', dom.g('main'));
+            this.redirect('/edit', dom.getData(itemDom, 'id'));
         }
 
     };
 
-    config.events = {
-        ready: function () {
-            var year = new Date().getFullYear();
-            var month = new Date().getMonth() + 1;
-            dom.query('.time').innerHTML = year + '年' + month + '月';
-            var timeBar = dom.query('.time-line');
-            dom.setStyle(timeBar, 'data-time', new Date().getTime());
+    // config.events = {
+    //     ready: function () {
+    //         var year = new Date().getFullYear();
+    //         var month = new Date().getMonth() + 1;
+    //         dom.query('.time').innerHTML = year + '年' + month + '月';
+    //         var timeBar = dom.query('.time-line');
+    //         dom.setStyle(timeBar, 'data-time', new Date().getTime());
 
-        }
-    };
+    //     }
+    // };
 
 
     return config;
