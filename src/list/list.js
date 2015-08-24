b4ac2275acc2c7cd0f8c362afb9720eac0d69775
unknown <fengchuyan@6C8F7A85617211B.internal.baidu.com>
2015-08-24 15:49:55 +0800

    list

--- src/list/list.js
+++ src/list/list.js
@@ -4,6 +4,7 @@
  */
 
 define(function (require) {
+    var dom = require('saber-dom');
 
     var config = {};
 
@@ -12,13 +13,20 @@ define(function (require) {
     config.model = require('./listModel');
 
     config.events = {
-        sleep: function () {
-            this.scrollTop = document.body.scrollTop;
-            this.activeDate = this.view.activeDate;
-        },
-        wakeup: function () {
-            document.body.scrollTop = this.scrollTop;
-            this.model.activeDate = this.activeDate;
+        // sleep: function () {
+        //     this.scrollTop = document.body.scrollTop;
+        //     this.activeDate = this.view.activeDate;
+        // },
+        // wakeup: function () {
+        //     document.body.scrollTop = this.scrollTop;
+        //     this.model.activeDate = this.activeDate;
+        // },
+        'view: delete': function (id) {
+            this.model.deleteItem(id).then(function (deletedItem) {
+                var typeDom = deletedItem.type === 'expense' ? dom.query('.expense-value') : dom.query('.income-value');
+                typeDom.innerHTML -= deletedItem.number;
+            });
+            
         }
     }
 
